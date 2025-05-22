b98806f2e90e700fa6fd11e2b66593a7894efd65
JBSL <bvkvgxvt@gmail.com>
2025-05-22 22:37:28 +0200

    Created validator class and added validations to the creation movie handler

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/tehweifu/greenlight/internal/data"
+	"github.com/tehweifu/greenlight/internal/data/validator"
 )
 
 // Add a createMovieHandler fo the "POST /v1/movies" endpoint. For now we simply
@@ -29,6 +30,24 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 	if err != nil {
 		// Use the new badRequestResponse() helper.
 		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	// Copy the values from the input struct to a new Movie struct.
+	movie := &data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
+
+	// Initialize a new Validator.
+	v := validator.New()
+
+	// Call the ValidateMovie() function and return a response containing the errors if
+	// any of the checks fail.
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
 
