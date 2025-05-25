c29fe1356e984419a0b4a7ce51aa4b6cbcb6f7a2
JBSL <bvkvgxvt@gmail.com>
2025-05-25 11:48:09 +0200

    Refactored update movie handler to implement optimistic locking using the version column

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -63,3 +63,8 @@ func (app *application) methodNotAllowedResponse(w http.ResponseWriter, r *http.
 func (app *application) failedValidationResponse(w http.ResponseWriter, r *http.Request, errors map[string]string) {
 	app.errorResponse(w, r, http.StatusUnprocessableEntity, errors)
 }
+
+func (app *application) editConflictResponse(w http.ResponseWriter, r *http.Request) {
+	message := "unable to update teh record due to an edit conflict, please try again"
+	app.errorResponse(w, r, http.StatusConflict, message)
+}
