a5d7d1472a0a0cffe022f156ad4e6b8ebce9dd06
JBSL <bvkvgxvt@gmail.com>
2025-06-07 10:43:22 +0200

    Added some custom metrics like database stats, goroutines and timestamp

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"context"
 	"database/sql"
+	"expvar"
 	"flag"
 	"log/slog"
 	"os"
+	"runtime"
 	"strings"
 	"sync"
 	"time"
@@ -128,6 +130,25 @@ func main() {
 	// Also log a message to say that the connection pool has been successfully
 	// established.
 	logger.Info("database connection pool established")
+
+	// Publish a new "version" variable in teh expvar handler containing our application
+	// version number (currently the constant "1.0.0").
+	expvar.NewString("version").Set(version)
+
+	// Publish the number of active goroutines.
+	expvar.Publish("goroutines", expvar.Func(func() any {
+		return runtime.NumGoroutine()
+	}))
+
+	// Publish the database connection pool statistics.
+	expvar.Publish("database", expvar.Func(func() any {
+		return db.Stats()
+	}))
+
+	// Publish the current Unix timestamp
+	expvar.Publish("timestamp", expvar.Func(func() any {
+		return time.Now().Unix()
+	}))
 
 	// Initialize a new Mailer instance using the settings from the command line
 	// flags, and add it to the application struct.
